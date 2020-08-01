b61601e4509e417fff27169202750c48fa23a143
Satvikkaushal <satvikkaushal8@gmail.com>
2020-08-01 11:34:48 +0530

    Added files via upload

---
+++ Assets/Scripts/PlaymodeTests/Tests.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+
+        clock m_clock;
+
+        GameObject m_HourNeedle, m_MinutesNeedle, m_SecondsNeedle, m_clockObject;
+
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            m_HourNeedle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            m_MinutesNeedle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            m_SecondsNeedle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            m_clockObject = GameObject.Instantiate(new GameObject());
+            m_clockObject.AddComponent<clock>();
+            var m_ClockComponent = m_clockObject.GetComponent<clock>();
+            m_ClockComponent.hoursTransform = m_HourNeedle.transform;
+            m_ClockComponent.minutesTransform = m_MinutesNeedle.transform;
+            m_ClockComponent.secondsTransform = m_SecondsNeedle.transform;
+        }
+        [UnityTest]
+        public IEnumerator SecondNeedleLocalRotationChangingEverySecondOnDiscrete()
+        {
+            var SecondNeedleLocalRotation = m_SecondsNeedle.transform.localRotation;
+            yield return new WaitForSeconds(1.0f);
+            var SecondNeedleNewLocalRotation = m_SecondsNeedle.transform.localRotation;
+            Assert.AreNotEqual(SecondNeedleLocalRotation, SecondNeedleNewLocalRotation);
+        }
+        [UnityTest]
+        public IEnumerator MinuteNeedleLocalRotationChangingAfterMinuteOnDiscrete()
+        {
+            var MinuteNeedleLocalRotation = m_MinutesNeedle.transform.localRotation;
+            yield return new WaitForSeconds(1.0f);
+            var MinuteNeedleNewLocalRotation = m_MinutesNeedle.transform.localRotation;
+            Assert.AreEqual(MinuteNeedleLocalRotation, MinuteNeedleNewLocalRotation);
+        }
+        [UnityTest]
+        public IEnumerator HourNeedleLocalRotationChangingAfterHourOnDiscrete()
+        {
+            var HourNeedleLocalRotation = m_HourNeedle.transform.localRotation;
+            yield return new WaitForSeconds(1.0f);
+            var HourNeedleNewLocalRotation = m_HourNeedle.transform.localRotation;
+            Assert.AreEqual(HourNeedleLocalRotation, HourNeedleNewLocalRotation);
+        }
+
+        [UnityTest]
+        public IEnumerator SecondNeedleLocalRotationChangingEverySecondOnContinous()
+        {
+            var m_ClockComponent = m_clockObject.GetComponent<clock>();
+            m_ClockComponent.continuous = true;
+            var SecondNeedleLocalRotation = m_SecondsNeedle.transform.localRotation;
+            yield return null;
+            var SecondNeedleNewLocalRotation = m_SecondsNeedle.transform.localRotation;
+            Assert.AreNotEqual(SecondNeedleLocalRotation, SecondNeedleNewLocalRotation);
+        }
+        [UnityTest]
+        public IEnumerator MinutesNeedleLocalRotationChangingEverySecondOnContinous()
+        {
+            var m_ClockComponent = m_clockObject.GetComponent<clock>();
+            m_ClockComponent.continuous = true;
+            var MinuteNeedleLocalRotation = m_MinutesNeedle.transform.localRotation;
+            yield return null;
+            var MinuteNeedleNewLocalRotation = m_MinutesNeedle.transform.localRotation;
+            Assert.AreNotEqual(MinuteNeedleLocalRotation, MinuteNeedleNewLocalRotation);
+        }
+
+        [UnityTest]
+        public IEnumerator HourNeedleLocalRotationChangingEverySecondOnContinous()
+        {
+            var m_ClockComponent = m_clockObject.GetComponent<clock>();
+            m_ClockComponent.continuous = true;
+            var HourNeedleLocalRotation = m_HourNeedle.transform.localRotation;
+            yield return null;
+            var HourNeedleNewLocalRotation = m_HourNeedle.transform.localRotation;
+            Assert.AreNotEqual(HourNeedleLocalRotation, HourNeedleNewLocalRotation);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/clock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clock : MonoBehaviour
+{
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+    const float degreesPerHour = 30f, degreesPerMinute = 6f, degreesPerSecond = 6f;
+    public bool continuous;
+    void UpdateContinuous()
+    {
+        Debug.Log("continous");
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        //Debug.Log("span" + (float)time.TotalHours);
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+
+        Debug.Log(hoursTransform.localRotation + "hoursTransform");
+
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        Debug.Log("minutes" + minutesTransform.localRotation);
+
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+        Debug.Log("seconds" + secondsTransform.localRotation);
+
+    }
+
+    void UpdateDiscrete()
+    {
+        Debug.Log("Dicrete");
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        Debug.Log(hoursTransform.localRotation + "hoursTransform");
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        Debug.Log("minutes" + minutesTransform.localRotation);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+        Debug.Log("seconds" + secondsTransform.localRotation);
+    }
+    public void Update()
+    {
+        Debug.Log("Update");
+        if (continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+}
